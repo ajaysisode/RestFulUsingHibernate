705b1772852a763a1dbb8874ee11c2b78f115472
ajaysisode <sisodiya.ajay85@gmail.com>
2022-08-12 04:20:20 -0700

    RES -1:create package Stucture

---
+++ RestFulUsingHibernate/src/main/java/com/bikkadit/RestFulUsingHibernate/RestFulUsingHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadit.RestFulUsingHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulUsingHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulUsingHibernateApplication.class, args);
+	}
+
+}
